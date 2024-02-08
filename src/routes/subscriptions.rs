fb70faf1346ffb0cf46a26df0bc37146277c06ba
Hunter Baldwin <huntercbaldwin98@gmail.com>
2024-02-07 20:47:40 -0800

    Fixed tests - database calls

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,11 +1,33 @@
 use actix_web::{web, HttpResponse};
+use chrono::Utc;
+use sqlx::PgPool;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
-    pub name: String,
-    pub email: String,
+    name: String,
+    email: String,
 }
 
-pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    return HttpResponse::Ok().finish();
+pub async fn subscribe(
+    form: web::Form<FormData>,
+    connection_pool: web::Data<PgPool>,
+) -> HttpResponse {
+    match sqlx::query!(
+        r#"INSERT INTO subscriptions (id, email, name, subscribed_at)
+    VALUES ($1, $2, $3, $4)"#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(connection_pool.get_ref())
+    .await
+    {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            println!("Failed to execute query: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,13 +1,16 @@
 use actix_web::{dev::Server, web, App, HttpServer};
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 use crate::routes::*;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection: PgPool) -> Result<Server, std::io::Error> {
+    let connection_pool = web::Data::new(connection);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(connection_pool.clone())
     })
     .listen(listener)?
     .run();
