422e79498f01ef122a85e3a950b00e0129e431f4
Hunter Baldwin <huntercbaldwin98@gmail.com>
2024-02-21 06:27:15 -0800

    Finished chapter 6 - subscription validation and testing

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -191,3 +191,33 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 
     app.drop().await;
 }
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_fields_are_present_but_empty() {
+    let mut app = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=&email=ursula_le_guin%40gmail.com", "name is empty"),
+        ("name=Ursula&email=", "email is empty"),
+        ("name=Ursula&email=definitely-not-an-email", "invalid email"),
+    ];
+
+    for (body, description) in test_cases {
+        let response = client
+            .post(format!("{}/subscriptions", app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not return a 400 BAD RESPONSE when the payload was {}.",
+            description
+        );
+    }
+
+    app.drop().await;
+}
